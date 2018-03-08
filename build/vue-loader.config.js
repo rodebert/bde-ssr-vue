2ab77f9fb09c0821048a22c0a83f9011bc91f910
Robert Weber <closingtag@gmail.com>
2018-03-08 17:15:49 +0100

    Add config for sass rendering

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,9 +1,22 @@
+const { THEME } = process.env;
+
 module.exports = {
-  extractCSS: process.env.NODE_ENV === 'production',
-  preserveWhitespace: false,
-  postcss: [
-    require('autoprefixer')({
-      browsers: ['last 3 versions']
-    })
-  ]
+	extractCSS: process.env.NODE_ENV === 'production',
+	preserveWhitespace: false,
+	postcss: [
+		require('autoprefixer')({
+		browsers: ['last 3 versions']
+	})],
+	loaders: {
+		scss: [
+			{ loader: 'vue-style-loader' },
+			{ loader: 'css-loader' },
+			{
+				loader: 'sass-loader',
+				options: {
+					data: `$theme: '${THEME}';`
+				}
+			}
+		]
+	}
 }
